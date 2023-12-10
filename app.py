e782246f7c487d52e9f95fd0568698247edf3b05
Dylan Lange <dylan@Michelles-MacBook-Air.local>
2023-12-10 21:27:40 +0100

    update

---
+++ app.py
@@ -0,0 +1,70 @@
+#All the code in here is used for basic Python learning purposes
+
+#Sample code for my first app
+print("Hello, World!")
+
+#another simple print example
+print("Another hello world example")
+#add 5 new lines
+print("\n"*5)
+
+#simple integer addition example
+print("Simple calc showing 1+1 answer: "); print(1+1)
+#add a new line
+print("\n")
+
+#simple example showing f string with string and integer
+print(f"Simple calc showing 2+2 answer: {2+2}")
+
+#create a new DICT with examples values
+my_dict = {"name":"John", "age":30, "city":"New York"}
+print("\n")
+print("Some values from DICT of my_dict:")
+print(my_dict)
+print("A DICT is a collection of key value pairs!")
+
+#create a new LIST with examples values
+my_list = ["John", 30, "New York"]
+print("\n")
+print("Some values from LIST of my_list:")
+print(my_list)
+print("A LIST is a collection of values!")
+
+#create a new TUPLE with examples values
+my_tuple = ("John", 30, "New York")
+print("\n")
+print("Some values from TUPLE of my_tuple:")
+print(my_tuple)
+print("A TUPLE is a collection of values which are immutable!")
+
+#create a new SET with examples values
+my_set = {"John", 30, "New York"}
+print("\n")
+print("Some values from SET of my_set:")
+print(my_set)
+print("A SET is a collection of values which are unique!")
+#add a new value to the SET
+my_set.add("Jane")
+print("\n")
+print("Some values from SET of my_set:")
+print(my_set)
+print("A SET is a collection of values which are unique!")
+
+#create a new STRING with examples values
+my_string = "John"
+print("\n")
+print("Some values from STRING of my_string:")
+print(my_string)
+print("A STRING is a collection of characters!")
+#add a new value to the STRING
+my_string += " " + "Doe"
+print("\n")
+print("Some values from STRING of my_string:")
+print(my_string)
+print("A STRING is a collection of characters!")
+
+
+
+
+
+
